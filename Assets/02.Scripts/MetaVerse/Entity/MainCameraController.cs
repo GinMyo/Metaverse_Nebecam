8e5ae151b07957ee4e2d549ded941d5cc45ff572
GinMyo <124099502+GinMyo@users.noreply.github.com>
2025-07-28 10:30:41 +0900

    [feat] 카메라 경계 설정, 타일맵 콜라이더 끼임 현상 해결
    1. Collision Tilemap 오브젝트에 Composite Collider 2D 컴포넌트 추가
    2. Rigidbody 2D 컴포넌트의 Body Type을 Static 으로 변경
    3. Tilemap Colider 2D 컴포넌트의 Used By Composite 체크
    4. Physics Material 2D 파일 추가해서 Fricion 0으로 맞춘 후 Player랑 Collision의Colider에 각각 추가

--- Assets/02.Scripts/MetaVerse/Entity/MainCameraController.cs
+++ Assets/02.Scripts/MetaVerse/Entity/MainCameraController.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class MainCameraController : MonoBehaviour
 {
     [SerializeField] Transform player;
+    [SerializeField] Vector2 minCameraBoundary;
+    [SerializeField] Vector2 maxCameraBoundary;
     private void LateUpdate()
     {
         Vector3 targetPos = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+
+        targetPos.x = Mathf.Clamp(targetPos.x, minCameraBoundary.x, maxCameraBoundary.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, minCameraBoundary.y, maxCameraBoundary.y);
+        
         transform.position = targetPos;
     }
 }
